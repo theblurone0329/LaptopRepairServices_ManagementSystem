d7552abc729092611be5970346e2d33e0aa95f8b
theblurone0329 <adrianfwl@gmail.com>
2022-06-19 17:15:42 +0800

    Finished with one button on billing page

--- Laptop Repair Services Management System/BillingandPayment.cs
+++ Laptop Repair Services Management System/BillingandPayment.cs
@@ -22,29 +22,58 @@ namespace Laptop_Repair_Services_Management_System
 
         private void btnFindCustomer_Click(object sender, EventArgs e)
         {
+            con.Open();
             string temp = txtEnterCustomerID.Text;
             List<char> charToRemove = new List<char>() { 'U'};
             int userID = Convert.ToInt32(temp.Filter(charToRemove));
 
-            SqlCommand cmd = new SqlCommand($"Select servName From BookedDetails Where userID = '{userID} And servStatus = 'Waiting for Payment';", con);
-            string servName = cmd.ExecuteScalar().ToString();
-            SqlCommand cmd1 = new SqlCommand($"Select username From BookedDetails Where userID = '{userID} And servStatus = 'Waiting for Payment' AND servName = '{servName}';", con);
-            string username = cmd1.ExecuteScalar().ToString();
-            SqlCommand cmd2 = new SqlCommand($"Select servType From BookedDetails Where userID = '{userID} And servStatus = 'Waiting for Payment' AND servName = '{servName}';", con);
-            string servType = cmd2.ExecuteScalar().ToString();
+            SqlCommand cmd = new SqlCommand($"Select Count(*) From BookedServices Where servStatus = 'Waiting for Payment' and userID = '{userID}';", con);
+            int count = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+            int curr = 1;
+            string servName;
+            string servType;
+            double Totalprice = 0;
             double price;
 
-            if (servType == "Urgent")
+            if (count != 0)
             {
-                SqlCommand cmd3 = new SqlCommand($"Select urgPrice From BookedDetails Where servName = '{servName}';", con);
-                price = Convert.ToDouble(cmd3.ExecuteScalar().ToString());
-            } else
-            {
-                SqlCommand cmd3 = new SqlCommand($"Select normPrice From BookedDetails Where servName = '{servName}';", con);
-                price = Convert.ToDouble(cmd3.ExecuteScalar().ToString());
+                count++;
+                while (curr != count)
+                {
+                    SqlCommand cmd1 = new SqlCommand($"Select Top({curr}) servName From BookedServices Where servStatus = 'Waiting for Payment' Order by servName ASC;", con);
+                    servName = cmd1.ExecuteScalar().ToString();
+                    SqlCommand cmd2 = new SqlCommand($"Update BookedServices Set servStatus = 'In List' Where servName = '{servName}' AND userID = '{userID}';", con);
+                    cmd2.ExecuteScalar();
+                    SqlCommand cmd3 = new SqlCommand($"Select servType From BookedServices Where userID = {userID} AND servStatus = 'In List' AND servName = '{servName}';", con);
+                    servType = cmd3.ExecuteScalar().ToString();
+                    lstCustomerBill.Items.Add(servName);
+                    if (servType == "Urgent")
+                    {
+                        SqlCommand cmd4 = new SqlCommand($"Select urgPrice From ServiceDetails Where servName = '{servName}';", con);
+                        price = Convert.ToDouble(cmd4.ExecuteScalar().ToString());
+                        Totalprice = price + Totalprice;
+
+                    }
+                    else
+                    {
+                        SqlCommand cmd4 = new SqlCommand($"Select normPrice From ServiceDetails Where servName = '{servName}';", con);
+                        price = Convert.ToDouble(cmd4.ExecuteScalar().ToString());
+                        Totalprice = price + Totalprice;
+                    }
+                    SqlCommand cmd5 = new SqlCommand($"Select username From AccountDetails Where userID = {userID};", con);
+                    string username = cmd5.ExecuteScalar().ToString();
+                    lblDisplayCustomerName.Text = username;
+                    curr++;
+                }
             }
-            lblDisplayTotalAmountPay.Text = $"RM {price}";
-            lblDisplayCustomerName.Text = username;
+            else
+            {
+                MessageBox.Show("No services booked from this customer...");
+                txtEnterCustomerID.Text = "";
+            }
+
+            lblDisplayTotalAmountPay.Text = $"RM {Totalprice}";
+            con.Close();
         }
     }
 }
