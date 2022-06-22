24ca93b786249afd464fa48995bf2e0cda4ef486
theblurone0329 <adrianfwl@gmail.com>
2022-06-22 21:27:00 +0800

    Admin Create acc all good!

--- Laptop Repair Services Management System/RegisterAccAdmin.cs
+++ Laptop Repair Services Management System/RegisterAccAdmin.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,7 @@ namespace Laptop_Repair_Services_Management_System
 {
     public partial class RegisterAccAdmin : Form
     {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDB"].ToString());
         public RegisterAccAdmin()
         {
             InitializeComponent();
@@ -19,10 +22,23 @@ namespace Laptop_Repair_Services_Management_System
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Account Registered!");
-            txtEmail.Clear();
-            txtPassword.Clear();
-            txtUsername.Clear();
+            con.Open();
+
+            if (txtUsername.Text == "" || txtPassword.Text == "" || txtEmail.Text == "")
+            {
+                MessageBox.Show("All Details must be filled in!");
+            }
+            else
+            {
+                string role = cbAccType.Text.ToLower();
+                SqlCommand cmd = new SqlCommand($"Insert into AccountDetails values('{txtUsername.Text}', '{txtPassword.Text}', '{txtEmail.Text}', '{role}')", con);
+                cmd.ExecuteScalar();
+                MessageBox.Show("Account Registered!");
+                txtEmail.Clear();
+                txtPassword.Clear();
+                txtUsername.Clear();
+            }
+            con.Close();
         }
     }
 }
