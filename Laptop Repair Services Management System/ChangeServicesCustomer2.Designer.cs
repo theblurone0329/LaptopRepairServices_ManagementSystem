0b4df836dfa63d93b868bac240a5c661a5e71461
theblurone0329 <adrianfwl@gmail.com>
2022-06-23 12:08:50 +0800

    Change design a bit of changeservicecustomer2

--- Laptop Repair Services Management System/ChangeServicesCustomer2.Designer.cs
+++ Laptop Repair Services Management System/ChangeServicesCustomer2.Designer.cs
@@ -29,7 +29,12 @@ namespace Laptop_Repair_Services_Management_System
         /// </summary>
         private void InitializeComponent()
         {
-            this.pnlAttachForm = new System.Windows.Forms.Panel();
+            this.lblCurrServType = new System.Windows.Forms.Label();
+            this.lblCurrServName = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblNormPrice = new System.Windows.Forms.Label();
+            this.lblUrgPrice = new System.Windows.Forms.Label();
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnConfirm = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
@@ -38,28 +43,72 @@ namespace Laptop_Repair_Services_Management_System
             this.radBtnNormal = new System.Windows.Forms.RadioButton();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.pnlAttachForm.SuspendLayout();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // pnlAttachForm
+            // lblCurrServType
             // 
-            this.pnlAttachForm.AutoScroll = true;
-            this.pnlAttachForm.BackColor = System.Drawing.Color.White;
-            this.pnlAttachForm.Controls.Add(this.btnCancel);
-            this.pnlAttachForm.Controls.Add(this.btnConfirm);
-            this.pnlAttachForm.Controls.Add(this.label2);
-            this.pnlAttachForm.Controls.Add(this.ComboBoxServiceType);
-            this.pnlAttachForm.Controls.Add(this.radBtnUrgent);
-            this.pnlAttachForm.Controls.Add(this.radBtnNormal);
-            this.pnlAttachForm.Controls.Add(this.label3);
-            this.pnlAttachForm.Controls.Add(this.label1);
-            this.pnlAttachForm.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlAttachForm.ForeColor = System.Drawing.Color.Black;
-            this.pnlAttachForm.Location = new System.Drawing.Point(0, 0);
-            this.pnlAttachForm.Margin = new System.Windows.Forms.Padding(2);
-            this.pnlAttachForm.Name = "pnlAttachForm";
-            this.pnlAttachForm.Size = new System.Drawing.Size(600, 366);
-            this.pnlAttachForm.TabIndex = 1;
+            this.lblCurrServType.AutoSize = true;
+            this.lblCurrServType.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrServType.Location = new System.Drawing.Point(229, 113);
+            this.lblCurrServType.Name = "lblCurrServType";
+            this.lblCurrServType.Size = new System.Drawing.Size(59, 19);
+            this.lblCurrServType.TabIndex = 54;
+            this.lblCurrServType.Text = "Normal";
+            // 
+            // lblCurrServName
+            // 
+            this.lblCurrServName.AutoSize = true;
+            this.lblCurrServName.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrServName.Location = new System.Drawing.Point(229, 78);
+            this.lblCurrServName.Name = "lblCurrServName";
+            this.lblCurrServName.Size = new System.Drawing.Size(312, 19);
+            this.lblCurrServName.TabIndex = 53;
+            this.lblCurrServName.Text = "Troubleshoot and fix computer running slow";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(56, 110);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 22);
+            this.label5.TabIndex = 52;
+            this.label5.Text = "Current Service Type: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(56, 75);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 22);
+            this.label4.TabIndex = 51;
+            this.label4.Text = "Current Service: ";
+            // 
+            // lblNormPrice
+            // 
+            this.lblNormPrice.AutoSize = true;
+            this.lblNormPrice.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNormPrice.Location = new System.Drawing.Point(239, 270);
+            this.lblNormPrice.Name = "lblNormPrice";
+            this.lblNormPrice.Size = new System.Drawing.Size(35, 19);
+            this.lblNormPrice.TabIndex = 50;
+            this.lblNormPrice.Text = "RM ";
+            // 
+            // lblUrgPrice
+            // 
+            this.lblUrgPrice.AutoSize = true;
+            this.lblUrgPrice.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUrgPrice.Location = new System.Drawing.Point(239, 243);
+            this.lblUrgPrice.Name = "lblUrgPrice";
+            this.lblUrgPrice.Size = new System.Drawing.Size(35, 19);
+            this.lblUrgPrice.TabIndex = 49;
+            this.lblUrgPrice.Text = "RM ";
             // 
             // btnCancel
             // 
@@ -67,14 +116,13 @@ namespace Laptop_Repair_Services_Management_System
             this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancel.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCancel.ForeColor = System.Drawing.Color.Black;
-            this.btnCancel.Location = new System.Drawing.Point(288, 256);
+            this.btnCancel.Location = new System.Drawing.Point(313, 311);
             this.btnCancel.Margin = new System.Windows.Forms.Padding(2);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(100, 34);
-            this.btnCancel.TabIndex = 20;
+            this.btnCancel.TabIndex = 48;
             this.btnCancel.Text = "CANCEL";
             this.btnCancel.UseVisualStyleBackColor = false;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // btnConfirm
             // 
@@ -82,50 +130,59 @@ namespace Laptop_Repair_Services_Management_System
             this.btnConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnConfirm.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnConfirm.ForeColor = System.Drawing.Color.Black;
-            this.btnConfirm.Location = new System.Drawing.Point(392, 256);
+            this.btnConfirm.Location = new System.Drawing.Point(417, 311);
             this.btnConfirm.Margin = new System.Windows.Forms.Padding(2);
             this.btnConfirm.Name = "btnConfirm";
             this.btnConfirm.Size = new System.Drawing.Size(100, 34);
-            this.btnConfirm.TabIndex = 19;
+            this.btnConfirm.TabIndex = 47;
             this.btnConfirm.Text = "CONFIRM";
             this.btnConfirm.UseVisualStyleBackColor = false;
-            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(28, 111);
+            this.label2.Location = new System.Drawing.Point(26, 189);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 22);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "Service Type:";
+            this.label2.Size = new System.Drawing.Size(157, 22);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "Available Services: ";
             // 
             // ComboBoxServiceType
             // 
+            this.ComboBoxServiceType.AutoCompleteCustomSource.AddRange(new string[] {
+            "Laptop Screen Replacement"});
             this.ComboBoxServiceType.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(238)))), ((int)(((byte)(245)))));
-            this.ComboBoxServiceType.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBoxServiceType.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ComboBoxServiceType.ForeColor = System.Drawing.Color.Black;
             this.ComboBoxServiceType.FormattingEnabled = true;
-            this.ComboBoxServiceType.Location = new System.Drawing.Point(144, 110);
+            this.ComboBoxServiceType.Items.AddRange(new object[] {
+            "Remove Virus, Malware, or Spyware",
+            "Troubleshoot and fix computer running slow",
+            "Laptop Screen Replacement",
+            "Laptop Battery Replacement",
+            "Operating System Format and Installation",
+            "Data Backup and Recovery",
+            "Laptop Keyboard Replacement",
+            "Internet Connectivity Issues"});
+            this.ComboBoxServiceType.Location = new System.Drawing.Point(187, 189);
             this.ComboBoxServiceType.Margin = new System.Windows.Forms.Padding(2);
             this.ComboBoxServiceType.Name = "ComboBoxServiceType";
-            this.ComboBoxServiceType.Size = new System.Drawing.Size(134, 27);
-            this.ComboBoxServiceType.TabIndex = 13;
-            this.ComboBoxServiceType.Text = "Service Type";
+            this.ComboBoxServiceType.Size = new System.Drawing.Size(263, 25);
+            this.ComboBoxServiceType.TabIndex = 43;
             // 
             // radBtnUrgent
             // 
             this.radBtnUrgent.AutoSize = true;
             this.radBtnUrgent.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radBtnUrgent.ForeColor = System.Drawing.Color.Black;
-            this.radBtnUrgent.Location = new System.Drawing.Point(144, 204);
+            this.radBtnUrgent.Location = new System.Drawing.Point(140, 266);
             this.radBtnUrgent.Margin = new System.Windows.Forms.Padding(2);
             this.radBtnUrgent.Name = "radBtnUrgent";
             this.radBtnUrgent.Size = new System.Drawing.Size(83, 26);
-            this.radBtnUrgent.TabIndex = 16;
+            this.radBtnUrgent.TabIndex = 46;
             this.radBtnUrgent.TabStop = true;
             this.radBtnUrgent.Text = "Normal";
             this.radBtnUrgent.UseVisualStyleBackColor = true;
@@ -135,11 +192,11 @@ namespace Laptop_Repair_Services_Management_System
             this.radBtnNormal.AutoSize = true;
             this.radBtnNormal.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radBtnNormal.ForeColor = System.Drawing.Color.Black;
-            this.radBtnNormal.Location = new System.Drawing.Point(144, 178);
+            this.radBtnNormal.Location = new System.Drawing.Point(140, 240);
             this.radBtnNormal.Margin = new System.Windows.Forms.Padding(2);
             this.radBtnNormal.Name = "radBtnNormal";
             this.radBtnNormal.Size = new System.Drawing.Size(79, 26);
-            this.radBtnNormal.TabIndex = 15;
+            this.radBtnNormal.TabIndex = 45;
             this.radBtnNormal.TabStop = true;
             this.radBtnNormal.Text = "Urgent";
             this.radBtnNormal.UseVisualStyleBackColor = true;
@@ -149,42 +206,71 @@ namespace Laptop_Repair_Services_Management_System
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(28, 178);
+            this.label3.Location = new System.Drawing.Point(24, 240);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(103, 22);
-            this.label3.TabIndex = 14;
-            this.label3.Text = "Service Fee:";
+            this.label3.Size = new System.Drawing.Size(112, 22);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Service Type:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(11, 18);
+            this.label1.Location = new System.Drawing.Point(5, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(269, 40);
-            this.label1.TabIndex = 11;
+            this.label1.TabIndex = 41;
             this.label1.Text = "CHANGE SERVICE";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 148);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(594, 19);
+            this.label6.TabIndex = 55;
+            this.label6.Text = "---------------------------------------------------------------------------------" +
+    "------------------------------------";
             // 
             // ChangeServicesCustomer2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.pnlAttachForm);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblCurrServType);
+            this.Controls.Add(this.lblCurrServName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblNormPrice);
+            this.Controls.Add(this.lblUrgPrice);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ComboBoxServiceType);
+            this.Controls.Add(this.radBtnUrgent);
+            this.Controls.Add(this.radBtnNormal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
             this.Name = "ChangeServicesCustomer2";
             this.Text = "ChangeServicesCustomer2";
-            this.pnlAttachForm.ResumeLayout(false);
-            this.pnlAttachForm.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel pnlAttachForm;
+        private System.Windows.Forms.Label lblCurrServType;
+        private System.Windows.Forms.Label lblCurrServName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblNormPrice;
+        private System.Windows.Forms.Label lblUrgPrice;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnConfirm;
         private System.Windows.Forms.Label label2;
@@ -193,5 +279,6 @@ namespace Laptop_Repair_Services_Management_System
         private System.Windows.Forms.RadioButton radBtnNormal;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
     }
 }
